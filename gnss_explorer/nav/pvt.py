fixed receiver clock bias units

gnss_explorer/nav/pvt.py
@@ -242,7 +242,7 @@ class PvtSolver:
 
         # Initial guess at the center of the earth
         p_hat_user = Vec3d()
-        t_receiver_bias_m = (self.t_clock_bias) if self.t_clock_bias else 0.0  # [m]
+        t_receiver_bias_s = (self.t_clock_bias) if self.t_clock_bias else 0.0  # [m]
 
         # Least squares iterations
         n_itr = 0
@@ -251,7 +251,10 @@ class PvtSolver:
             p_svs = [Vec3d(*m.ephemerides.as_ecef(t=m.t_tx)) for m in measurements]
 
             rho_hat = np.array(
-                [np.linalg.norm(p_sv - p_hat_user) - t_receiver_bias_m for p_sv in p_svs]
+                [
+                    np.linalg.norm(p_sv - p_hat_user) + t_receiver_bias_s * nav.C_GPS
+                    for p_sv in p_svs
+                ]
             )
 
             rho = np.array([m.pseudorange for m in measurements])
@@ -267,11 +270,11 @@ class PvtSolver:
 
             v = rho_delta - h @ p_hat_user_update
 
-            t_receiver_bias_m += p_hat_user_update[3] / nav.C_GPS
+            t_receiver_bias_s += p_hat_user_update[3] / nav.C_GPS
             n_itr += 1
         print(
             f"ECEF: {p_hat_user}",
-            f"Clock bias: {t_receiver_bias_m / nav.C_GPS:15.9f} sec",
+            f"Clock bias: {t_receiver_bias_s:15.9f} sec",
         )
 
         print(
@@ -281,7 +284,7 @@ class PvtSolver:
         )
 
         self.p_ecef = p_hat_user
-        self.t_clock_bias = t_receiver_bias_m / nav.C_GPS
+        self.t_clock_bias = t_receiver_bias_s
 
     def _get_prns_by_c_n0(self) -> list[int]:
         """Return list of PRNs sorted by descending average C/N0."""